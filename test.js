2e64684152d9a825feee6dad5998bc96cdc20cbf
sungylee <sungylee@yahoo.com>
2018-11-09 16:24:58 -0500

    updated test.js for multiplication

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+
+//test for adding
 describe('addValue', function() {
   it('should return the sum of two whole numbers', function() {
     expect(addValue(2, 4)).to.equal(6);
@@ -16,6 +18,7 @@ describe('addValue', function() {
   });
 });
 
+//test for subtraction
 describe('subValue', function() {
   it('should return the diff of two whole numbers', function() {
     expect(subValue(2, 4)).to.equal(-2);
@@ -35,21 +38,22 @@ describe('subValue', function() {
   });
 });
 
-  describe('mulValue', function() {
-    it('should return the div of two whole numbers', function() {
-      expect(mulValue(2, 4)).to.equal(8);
-    });
-  
-    it('should return the div of two decimal numbers', function() {
-      expect(mulValue(2.5, 1.5)).to.equal(3.75);
-    });
-  
-    it('should return the div of a decimal number and a string that is coercible to a number', function() {
-      expect(mulValue(2.5, '1.5')).to.equal(3.75);
-    });
-
-    it('should return the original total for invalid inputs', function() {
-    expect(mulValue(4, 'cow')).to.equal(4);
+//Test for multiplication
+describe('mulValue', function() {
+  it('should return the multi of two whole numbers', function() {
+    expect(mulValue(2, 4)).to.equal(8);
   });
+
+  it('should return the multi  of two decimal numbers', function() {
+    expect(mulValue(2.5, 1.5)).to.equal(3.75);
+  });
+
+  it('should return the multi  of a decimal number and a string that is coercible to a number', function() {
+    expect(mulValue(2.5, '1.5')).to.equal(3.75);
+  });
+
+  it('should return the original total for invalid inputs', function() {
+  expect(mulValue(4, 'cow')).to.equal(4);
+});
 
 });
